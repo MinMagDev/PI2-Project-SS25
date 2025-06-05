cdaa131eb8842078ac0d80aa9de6b6802185a77b
minmagdev <levi.drieling@gmail.com>
2025-06-05 15:48:05 +0200

    Different DNA getter Functions

--- src/Genom/DNA.java
+++ src/Genom/DNA.java
@@ -1,11 +1,22 @@
 package Genom;
 
+import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class DNA {
     private List<Nucleotid> dna;
+
+    private final int SPEED_POSITION = 0;
+    private final int FORCEFIELD_RADIUS_POSITION = 6;
+    private final int INTERACTION_POSITION = 12;
+
+    private final int MAX_SPEED = 2;
+    private final int MAX_FIELD_RADIUS = 10;
+
+    private final double ZERO_SPEED_THREASHOLD = 0.3d;
+
 
     public DNA() {
         dna = generateRandomDNA(128);
@@ -25,6 +36,79 @@ public class DNA {
             result.add(nucVals[nuc]);
         }
         return result;
+    }
+
+    /**
+     * Reads the Speed from the dna
+     * @return speeeed
+     */
+    public double getSpeed(){
+        double result = 0.0d;
+        result += getIntValue(SPEED_POSITION, SPEED_POSITION+6);
+        result *= MAX_SPEED/4095d;
+        if (result <= ZERO_SPEED_THREASHOLD) return 0.0d;
+        return result;
+    }
+
+    /**
+     * Returns the Radius for the Force Field from the dna
+     * @return forcefield Radius
+     */
+    public double getRadius(){
+        double result = 0.0d;
+        result += getIntValue(FORCEFIELD_RADIUS_POSITION, FORCEFIELD_RADIUS_POSITION+6);
+        result *= MAX_FIELD_RADIUS / 4095d;
+        return result;
+    }
+
+    public int getIntValue(int start, int end){
+        int result = 0;
+        for(int i = start; i < end; i++){
+            int power =  end - i - 1;
+            result += dna.get(i).ordinal() * (int)Math.pow(4,power);
+        }
+        return result;
+    }
+
+    /**
+     * Returns how the species should interact to other species
+     * @param species The Species to interact with
+     * @return -1: Flee, 0: ignore, 1: Hunt
+     */
+    public int getInteraction(int species){
+        Nucleotid nuc = dna.get(INTERACTION_POSITION + species);
+        switch (nuc) {
+            case A -> {
+                return -1;
+            }
+            case C, G -> {
+                return 0;
+            }
+            case T -> {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Gets the color of the particle
+     * @return The color of the particle
+     */
+    public Color getColor(){
+        int dnaLength = dna.size();
+        int parts = (int)dnaLength/3;
+        int r = getIntValue(0,parts);
+        int g = getIntValue(parts, 2*parts);
+        int b = getIntValue(2*parts,dnaLength);
+        r *= (int) (255.0 /getMaxValue(parts));
+        g *= (int) (255.0 /getMaxValue(parts));
+        b *= (int) (255.0 /getMaxValue(parts));
+        return new Color(r,g,b);
+    }
+
+    public static int getMaxValue(int pow) {
+        return (int)Math.pow(4,pow)-1;
     }
 
     /** Generates a String represenation of a DNA object
--- src/Genom/DNATest.java
+++ src/Genom/DNATest.java
@@ -4,5 +4,7 @@ public class DNATest {
     public static void main(String[] args) {
         DNA dna1 = new DNA();
         dna1.print();
+        System.out.println(dna1.getSpeed());
+        System.out.println(DNA.getMaxValue(6));
     }
 }
